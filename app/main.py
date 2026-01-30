686e839a2665d5e7143f015b5c0d89984c9293ff
tejaskapse1902 <tejaskapse1902@gmail.com>
2026-01-30 16:57:29 +0530

    Model load on startup

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from app.api.routes import router
 from app.services.index_manager import initialize_index, start_auto_refresh
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.services.recommender import get_model
+
 app = FastAPI(title="Job Recommendation API")
 
 app.add_middleware(
@@ -18,5 +20,8 @@ app.include_router(router)
 
 @app.on_event("startup")
 def startup_event():
+    print("🚀 Starting up Job Recommendation API...")
+    get_model()  # Preload model at startup
     initialize_index()
     start_auto_refresh(900)   # 15 minutes
+    print("✅ Startup complete.")
